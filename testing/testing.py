d101064f42fcd32771f771afa434ebba3392dbbb
Marcus Nilszén <nilszen.marcus@gmail.com>
2025-04-20 11:00:10 +0200

    test script

---
+++ testing/testing.py
@@ -0,0 +1,31 @@
+import os
+import torch
+import torchvision
+import torch.nn as nn
+from torchvision.models import efficientnet_b0, EfficientNet_B0_Weights
+from torchvision.models.feature_extraction import create_feature_extractor
+import cv2
+import matplotlib.pyplot as plt
+
+import sys
+sys.path.append("../../ultralytics")
+from ultralytics import YOLO
+
+
+data_file = "data.yaml"
+
+with open(data_file, "w") as f:
+    f.write("""test: ../../seaDronesSee_yolo_testing/images/test
+nc: 1
+names: ["human"]""")
+
+model = YOLO("/kaggle/working/model/weights/best.pt")
+
+for i in range(3):
+    metrics = model.val(
+        data=data_file,
+        split="test"
+    )
+
+    print("Round:", i)
+    print(metrics)
